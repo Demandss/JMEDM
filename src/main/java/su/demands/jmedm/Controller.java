88e3818494b83626dc53c53fc329d008224115a9
Demandss <columbineroleplay@yandex.ru>
2021-11-17 12:10:47 +0300

    it's work!

--- src/main/java/su/demands/jmedm/Controller.java
+++ src/main/java/su/demands/jmedm/Controller.java
@@ -7,7 +7,9 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.net.URL;
+import java.util.Arrays;
 import java.util.List;
+import java.util.OptionalDouble;
 import java.util.stream.Collectors;
 
 public class Controller {
@@ -21,7 +23,7 @@ public class Controller {
     List<String> foundUniqueOperands;
 
     String[][] dataForFormulasArray;
-    int i = -1, j = 0;
+    int i = 0, j = 0;
     public Controller() {
         inputData = getDataFromResource("inputdata.txt")
                 .stream().mapToDouble(Double::parseDouble).boxed().toList();
@@ -31,15 +33,37 @@ public class Controller {
         dataForFormulasArray = new String[dataForFormulas.size()][3];
 
         dataForFormulas.forEach(s -> {
-            i++;
             for (String var : s.split("\\|")) {
-                dataForFormulasArray[i][j++] = var;
+                dataForFormulasArray[i][j++] = var.replaceAll(" ","");
                 if (j == 3)
                     j = 0;
             }
+            i++;
         });
+    }
 
-        System.out.println(getFn(36));
+    public double getTimeUntilTestingIsCompleted(int modifier) {
+        double sum = 0.0d;
+        for (int k = inputData.size(); k < inputData.size() + modifier; k++) {
+           sum += getXnPlusOne(k);
+        }
+        return sum;
+    }
+
+    public double getTotalTestingTime(int modifier) {
+        return getTimeUntilTestingIsCompleted(modifier) + sumOffDataBottom();
+    }
+
+    private double getXnPlusOne(int n) {
+        return 1 / (getK() * (getB() - n));
+    }
+
+    private double getK() {
+        return inputData.size() / ((getB() + 1) * sumOffDataBottom() - sumOffDataTop());
+    }
+
+    private int getB() {
+        return getMinGnDifferenceFn() - 1;
     }
 
     private double sumOffDataTop() {
@@ -61,9 +85,28 @@ public class Controller {
     private double getFn(int m) {
         double sum = 0;
         for (int i = 0; i < inputData.size(); i++) {
-            sum = 1.0D / (m - (i+1));
+            sum += 1.0D / (m - (i+1));
         }
         return sum;
+    }
+
+    private int getMinGnDifferenceFn() {
+        double[] nums = new double[dataForFormulas.size()];
+        for (int k = 0; k < dataForFormulas.size(); k++) {
+            for (int j = inputData.size(); j < inputData.size() + dataForFormulas.size(); j++) {
+                nums[k] = Math.max(
+                        Math.abs(Double.parseDouble(dataForFormulasArray[k][2]) - getGnDifferenceFn(j)),
+                        Double.parseDouble(dataForFormulasArray[k][2])
+                );
+            }
+        }
+        int number = 0;
+        for (int i1 = 0; i1 < dataForFormulasArray.length; i1++) {
+            if (Double.parseDouble(dataForFormulasArray[i1][2]) == Arrays.stream(nums).min().getAsDouble()) {
+                number = inputData.size() + i1;
+            }
+        }
+        return number;
     }
 
     private double getGnDifferenceFn(int m) {
@@ -88,5 +131,14 @@ public class Controller {
             return new BufferedReader(new FileReader(new File(resource.toURI()))).lines()
                     .map(String::toLowerCase).collect(Collectors.toList());
         }
+    }
+
+    public boolean isInteger(String s) {
+        try {
+            Integer.parseInt(s);
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
     }
 }
--- src/main/java/su/demands/jmedm/Main.java
+++ src/main/java/su/demands/jmedm/Main.java
@@ -1,8 +1,66 @@
 package su.demands.jmedm;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 public class Main {
 
     public static void main(String[] args) {
         Controller controller = new Controller();
+
+        JFrame form = new JFrame("HalsteadMetrix");
+
+        form.setResizable(false);
+
+        form.setSize(220, 100);
+        form.setLayout(null);
+        form.setLocation(300,200);
+        form.getContentPane().setBackground(Color.decode("#EEEEEE"));
+
+        form.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+        form.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                int confirm = JOptionPane.showOptionDialog(form,
+                        "Close?", "Exit", JOptionPane.YES_NO_OPTION,
+                        JOptionPane.QUESTION_MESSAGE, null, null, null);
+                if (confirm == JOptionPane.YES_OPTION) {
+                    System.exit(1);
+                    form.dispose();
+                }
+            }
+        });
+
+        JLabel labelKey = new JLabel("Modifier:");
+        labelKey.setBounds(25,2,50, 20);
+        form.getContentPane().add(labelKey);
+
+        final JTextArea calcModifier = new JTextArea(10, 1);
+        calcModifier.setBounds(80,2,100,20);
+        form.getContentPane().add(calcModifier);
+
+        JButton buttonCalc = new JButton("Calculate");
+        buttonCalc.setBounds(25,30,150, 20);
+
+        buttonCalc.addActionListener(e -> {
+            if (controller.isInteger(calcModifier.getText())) {
+                int modifier = Integer.parseInt(calcModifier.getText());
+                JOptionPane.showMessageDialog(form,
+                        String.format("Время до завершения тестирования: %.2f \n Общее время тестирования: %.0f",
+                                controller.getTimeUntilTestingIsCompleted(modifier),
+                                Math.floor(controller.getTotalTestingTime(modifier)))
+                        ,"Calculations",
+                        JOptionPane.INFORMATION_MESSAGE,null);
+            } else {
+                JOptionPane.showMessageDialog(form,
+                        "Enter the modifier!","ERROR",
+                        JOptionPane.ERROR_MESSAGE,null);
+            }});
+
+        form.getContentPane().add(buttonCalc);
+
+        form.setVisible(true);
     }
 }
